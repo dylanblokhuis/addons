14bd6b3e20b23c0f0945713a1cf42aca0c0ae437
dylanblokhuis <dylanblokhuis3@hotmail.com>
2020-06-22 23:16:56 +0200

    init boilerplate

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import { render } from 'react-dom';
+import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
+import './app.global.css';
+
+const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
+
+document.addEventListener('DOMContentLoaded', () =>
+  render(
+    <AppContainer>
+      <div>Addons</div>
+    </AppContainer>,
+    document.getElementById('root')
+  )
+);
